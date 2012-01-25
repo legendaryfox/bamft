ae16162b200259d8ce305cbfd20dfc13eb32e87a
legendaryfox <legendaryfox@gmail.com>
2012-01-24 22:58:45 -0500

    Added basic homescreen functionality

--- gen/com/ksj/bamft/R.java
+++ gen/com/ksj/bamft/R.java
@@ -11,11 +11,29 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int food_truck_blue=0x7f020000;
+        public static final int food_truck_green=0x7f020001;
+        public static final int food_truck_icon=0x7f020002;
+        public static final int food_truck_red=0x7f020003;
+        public static final int food_truck_yellow=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int sample_0=0x7f020006;
+        public static final int sample_1=0x7f020007;
+        public static final int sample_2=0x7f020008;
+        public static final int sample_3=0x7f020009;
+        public static final int sample_4=0x7f02000a;
+        public static final int sample_5=0x7f02000b;
+        public static final int sample_6=0x7f02000c;
+        public static final int sample_7=0x7f02000d;
+        public static final int sample_images=0x7f02000e;
+    }
+    public static final class id {
+        public static final int gridview=0x7f050000;
     }
     public static final class layout {
         public static final int list_item=0x7f030000;
         public static final int main=0x7f030001;
+        public static final int truck_list=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
