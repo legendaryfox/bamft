85c433ea20378afbed0040d50fe6aa0dba94f7e9
Josiah <legendaryfox@gmail.com>
2012-02-11 01:49:19 +0800

    Cleaned up threading for Yelp

--- src/com/ksj/bamft/activity/TruckYelpListActivity.java
+++ src/com/ksj/bamft/activity/TruckYelpListActivity.java
@@ -46,145 +46,94 @@ public class TruckYelpListActivity extends ListActivity {
 	private static String downloadYelpFeed;
 	private static String yelpHandle;
 	private static ProgressDialog dialog;
-	
-	
+
+
 	private static ArrayList<YelpReview> yelpReviewItems;
 	private static Handler handler;
 	private static Thread downloadYelpThread;
-	private YelpRunnable myYelpRunnable = new YelpRunnable();
+	private YelpRunnable yelpRunnable = new YelpRunnable();
+
+	/*
+	 * NOTE: There's a lot of weird voodoo here because of the way we need to pass adapters, but how they don't work within a static class.
+	 */
 
 	public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 
-		// Extract info from bundle
+		// Extract info from bundles
 
 		Bundle extras = this.getIntent().getExtras();
 		yelpHandle = (String) extras.get(Constants.YELP_HANDLE);
-		
-		
-		
-		//BEGIN THREAD
-		// Create a handler to update the UI
+
 		handler = new Handler();
-		//myYelpRunnable.setListView(R.id.yelpReviewList);
-		myYelpRunnable.setListView(getListView());
-		Log.d("THREAD", "lv is currently set to " + myYelpRunnable.getListView() + " but it should be " + getListView());
-		/*
-		// get the latest imageView after restart of the application
-		imageView = (ImageView) findViewById(R.id.imageView1);
-		// Did we already download the image?
-		if (downloadBitmap != null) {
-			imageView.setImageBitmap(downloadBitmap);
+		yelpRunnable.setListView(getListView());
+
+		// Check to see if we already have the information
+		if (yelpReviewItems != null) {
+			YelpReviewItemAdapter adapter = new YelpReviewItemAdapter(TruckYelpListActivity.this.getBaseContext(), R.layout.yelp_review_item, yelpReviewItems);
+			ListView lv = getListView();
+			lv.setAdapter(adapter);
 		}
-		*/
+
 		// Check if the thread is already running
 		downloadYelpThread = (Thread) getLastNonConfigurationInstance();
 		if (downloadYelpThread != null && downloadYelpThread.isAlive()) {
 			dialog = ProgressDialog.show(this, "Yelp", "Accessing Yelp reviews....");
 		}
-		
+
 		downloadYelpItems();
-		/*
-		YelpReviewItemAdapter adapter = new YelpReviewItemAdapter(this, R.layout.yelp_review_item, getYelpReviewItems(yelpHandle));
-		setListAdapter(adapter);
-
-		ListView lv = getListView();
-		lv.setTextFilterEnabled(true);
-		*/
-		/*
-		lv.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				//When clicked, show a toast with the TextView text
-				Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
-			}
-		});
-		 */
 
 	}
-	
+
 	public void downloadYelpItems() {
+		// Begin the long process, start the dialog box
 		dialog = ProgressDialog.show(this, "Yelp", "Accessing Yelp reviews....");
-		downloadYelpThread = new DownloadYelpThread(myYelpRunnable);
-		Log.d("THREAD", "my yelp runnable lv is now " + myYelpRunnable.listView);
+		
+		downloadYelpThread = new DownloadYelpThread(yelpRunnable);
 		downloadYelpThread.start();
-	}
-	
 
-	// Save the thread
-	@Override
-	public Object onRetainNonConfigurationInstance() {
-		return downloadYelpThread;
 	}
 
 
-	static public class DownloadYelpThread extends Thread {
-		
-		private final YelpRunnable yelpRunnable;
-		
-		
+
+
+	static private class DownloadYelpThread extends Thread {
+
+		private final YelpRunnable myYelpRunnable;
+
+		/*
+		 * VOODOO: We need to do this because of the way Adapters work within the whole Static/Nonstatic framework.
+		 * This is why we need to repass the above "normal" yelpRunnable into this *final* yelpRunnable.
+		 */
 		public DownloadYelpThread(YelpRunnable yelpRunnable) {
-			this.yelpRunnable = yelpRunnable;
-			//this.start();
+			this.myYelpRunnable = yelpRunnable;
 		}
-		
+
 		@Override
 		public void run() {
-			Yelp yelp = new Yelp();
-			/*
-			try {
-				//new Thread().sleep(5000);
-				Log.d("THREAD", "I am ACTUALLYW AITING");
-			} catch (InterruptedException e) {
-				Log.d("THREAD", "I am waiting EXCEPTION");
-				e.printStackTrace();
-			}
-			*/
-			Log.d("THREAD", "I am done waiting.");
-			//downloadYelpFeed = yelp.getBusinessInfo(yelpHandle);
-			yelpReviewItems = getYelpReviewItems(yelpHandle);
-			handler.post(yelpRunnable);
-			Log.d("THREAD", "I should've posted runnable to handler by now.");
+			yelpReviewItems = getYelpReviewItems(yelpHandle); // <-- this is the longest part.
+			handler.post(myYelpRunnable); // <-- once we finish the above long part, we can run the runnable part.
 
 		}
 	}
 
 	private class YelpRunnable implements Runnable {
-		
-		public ListView listView;
-		//public ArrayList<YelpReview> yelpReviewItems;
-		
+
+		private ListView listView;
+
 		public void setListView(ListView listView) {
-			//this.listView = (ListView) TruckYelpListActivity.findViewById(id);
 			this.listView = listView;
 		}
-		
-		
-		public ListView getListView() {
-			return this.listView;
-		}
-		
-		public void run() {
-			//yelpReviewItems = getYelpReviewItems
-			Log.d("THREAD", "I am in runnable. About to define adapter");
-			YelpReviewItemAdapter adapter = new YelpReviewItemAdapter(TruckYelpListActivity.this.getBaseContext(), R.layout.yelp_review_item, yelpReviewItems);
-			Log.d("THREAD", "Adapter defined. About to set. Adapter is " + adapter.toString() + "and list view is " + listView + " and yelpReview is " + yelpReviewItems);
-			
-			listView.setAdapter(adapter);
-			Log.d("THREAD", "Adapter set.");
-			//setListAdapter(adapter);
 
-			//ListView listView = getListView();
-			//lv.setTextFilterEnabled(true);
+		public void run() {
+			YelpReviewItemAdapter adapter = new YelpReviewItemAdapter(TruckYelpListActivity.this.getBaseContext(), R.layout.yelp_review_item, yelpReviewItems);
+			listView.setAdapter(adapter);
+			
+			// Finished loading, dismiss the dialog
 			dialog.dismiss();
 		}
 	}
-
-
-
-
-
-
 
 	static public ArrayList<YelpReview> getYelpReviewItems(String yelpHandle) {
 
@@ -218,12 +167,18 @@ public class TruckYelpListActivity extends ListActivity {
 
 
 		} catch (Exception e) {
+			//TODO: print something to show no yelp entries exist.
 			e.printStackTrace();
 		}
 
 
 		return list;
 
+	}
+	// Save the thread
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+		return downloadYelpThread;
 	}
 
 
